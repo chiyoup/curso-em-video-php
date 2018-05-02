3d50062c31af7da03e7e355fc05450e6385432ce
chiyoup <chiyo.up@gmail.com>
2018-05-02 19:57:31 -0300

    Aula 04

---
+++ aula04/tipos.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="_css/estilo.css">
+    <title></title>
+  </head>
+  <body>
+    <div>
+      <?php
+        $idade = 21;
+        $nome = "Chiyo";
+
+        echo "Olá, meu nome é $nome tenho $idade anos!";
+      ?>
+    </div>
+  </body>
+</html>
