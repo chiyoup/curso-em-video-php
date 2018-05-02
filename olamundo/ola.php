bbfc9f65b00c16d821bd00411739f5ca8faf271e
chiyoup <chiyo.up@gmail.com>
2018-05-02 19:26:13 -0300

    Hello World PHP

---
+++ olamundo/ola.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <title>Primeiro Exemplo PHP</title>
+    <style>
+      h2 {
+        color: #45B9F5;
+        text-shadow: 1px 1px 1px black;
+      }
+    </style>
+  </head>
+  <body>
+    <h1>Testando PHP</h1>
+    <?php
+      echo "<h2>Olá, mundo!<h2>";
+    ?>
+  </body>
+</html>
